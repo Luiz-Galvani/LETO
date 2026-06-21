9a5ac5380d20d6d46311805b7aec526b38c12265
Kauã Henrique <kauahloliveria15@gmail.com>
2026-06-21 14:45:16 -0300

    Ajusta user CRUD: usa id,name,email,senha e corrige repository

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -6,92 +6,55 @@ dotenv.config()
 export class UserRepository {
     constructor(sqlConnection) {
         this.sql = sqlConnection
-    }//funções de acesso ao banco de dados
+    }
 
     // Create USER
     async create(userData) {
-        const {
-            cpf,
-            email,
-            phone,
-            password,
-            first_name,
-            last_name,
-            birthday,
-            gender
-        } = userData
+        const { id, name, email, senha } = userData
 
-        const result = await this.sql
-        ` 
-            INSERT INTO users (
-                cpf,
+        const result = await this.sql`
+            INSERT INTO "user" (
+                id,
+                name,
                 email,
-                phone,
-                password,
-                first_name,
-                last_name,
-                birthday,
-                gender
-            )
-
-            VALUES (
-                ${cpf},
+                senha
+            ) VALUES (
+                ${id},
+                ${name},
                 ${email},
-                ${phone},
-                ${password},
-                ${first_name},
-                ${last_name},
-                ${birthday},
-                ${gender}
-            )
-
-            RETURNING *`
+                ${senha}
+            ) RETURNING *`
 
         return result[0]
-    } //cria um elemento na tabela users
+    }
 
     // Get USERS
     async find() {
-        return await this.sql
-        `SELECT * FROM users`
+        return await this.sql`SELECT * FROM "user"`
     }
 
     // Get USER por ID
     async findOne(id) {
+        const result = await this.sql`SELECT * FROM "user" WHERE id = ${id}`
+        return result[0]
+    }
 
-        const result = await this.sql
-        ` SELECT * FROM users
-          WHERE id = ${id}`
-
+    async findByEmail(email) {
+        const result = await this.sql`
+            SELECT * FROM "user" WHERE email = ${email}`
         return result[0]
     }
 
     // Update USER
     async update(id, userData) {
+        const { name, email, senha } = userData
 
-        const {
-            cpf,
-            email,
-            phone,
-            password,
-            first_name,
-            last_name,
-            birthday,
-            gender
-        } = userData
-
-        const result = await this.sql
-        ` UPDATE users
+        const result = await this.sql`
+            UPDATE "user"
             SET
-                cpf = ${cpf},
+                name = ${name},
                 email = ${email},
-                phone = ${phone},
-                password = ${password},
-                first_name = ${first_name},
-                last_name = ${last_name},
-                birthday = ${birthday},
-                gender = ${gender}
-
+                senha = ${senha}
             WHERE id = ${id}
             RETURNING *`
 
@@ -100,15 +63,8 @@ export class UserRepository {
 
     // Delete USER
     async remove(id) {
-
-        const result = await this.sql
-        `
-            DELETE FROM users
-            WHERE id = ${id}
-            RETURNING *
-
-        `
-
+        const result = await this.sql`
+            DELETE FROM "user" WHERE id = ${id} RETURNING *`
         return result[0]
     }
 }
